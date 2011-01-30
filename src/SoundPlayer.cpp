464ed69c2499cec3810aa16e0b9e606551044090
hagino3000 <hagino.3000@gmail.com>
2011-01-30 19:19:47 +0900

    Remove sounds

--- src/SoundPlayer.cpp
+++ src/SoundPlayer.cpp
@@ -16,36 +16,8 @@ SoundPlayer::SoundPlayer() {
 	isDetecting = false;
 	
 	airSoundsNum = 5;
-	std::string airSoundsData[5] = {
-		"sounds/air/33796__yewbic__ambience03.wav",
-		"sounds/air/20705__Elektrocell__spacedrone3.wav",
-		"sounds/air/2855__nathanshadow__horror_ambient_001.wav",
-		"sounds/air/575__aarondbaron__messed.mp3",
-		"sounds/air/33796__yewbic__ambience03.wav"};
 	
-	for (int i=0; i<airSoundsNum; i++) {
-		ofSoundPlayer * p = new ofSoundPlayer();
-		p->loadSound(airSoundsData[i]);
-		p->setVolume(0.1);
-		airSounds.push_back(p);
-	}
-	currentPlayPosition = 0;
-	
-	// sounds
-	soundEffect1.loadSound("sounds/effect/509__Dante_Tanzi__GIRMI.wav");
-	soundArakawaWorld.loadSound("sounds/air2/5527__andreas__ambiens_1.mp3");
-	soundArakawaWorld.setLoop(true);
-	
-	soundNotify1.loadSound("sounds/notify/63915__liliangorini__ciudad.mp3");
-	soundNotify2.loadSound("sounds/notify/453__oyenstikker__subbass_with_noise.wav");
-	soundNotify1.setPan(-1);
-	soundNotify2.setPan(1);
-	soundNotify1.setLoop(true);
-	soundNotify2.setLoop(true);
-	
-	soundEffect1.setVolume(0.5);
-	soundNotify1.setVolume(0.1);
-	soundNotify2.setVolume(0.1);
+	// TODO
 }
 
 SoundPlayer::~SoundPlayer(){}
@@ -55,6 +27,7 @@ void setVol(ofSoundPlayer *p, float vol, float minimum=0.0f, float maximun=1.0f)
 }
 
 void SoundPlayer::update() {
+	/*
 	ofSoundUpdate();
 	
 	ofSoundPlayer * curSound = airSounds[currentPlayPosition];
@@ -88,13 +61,15 @@ void SoundPlayer::update() {
 		currentPlayPosition = (currentPlayPosition + 1) % airSoundsNum;
 		std::cout << "Sound Change!! " + ofToString(currentPlayPosition) << endl;
 	}
+	*/
 }
 
 void SoundPlayer::play() {
-	airSounds[currentPlayPosition]->play();
+	//airSounds[currentPlayPosition]->play();
 }
 
 void SoundPlayer::playNotify() {
+	/*
 	isNotifying = true;
 	if (soundNotify1.getIsPlaying() == false) {
 		soundNotify1.play();
@@ -102,9 +77,11 @@ void SoundPlayer::playNotify() {
 	if (soundNotify2.getIsPlaying() == false) {
 		soundNotify2.play();
 	}
+	*/
 }
 
 void SoundPlayer::stopNotify() {
+	/*
 	isNotifying = false;
 	if (soundNotify1.volume < 0.05) {
 		soundNotify1.stop();
@@ -112,19 +89,24 @@ void SoundPlayer::stopNotify() {
 	if (soundNotify2.volume < 0.05) {
 		soundNotify2.stop();
 	}
+	*/
 }
 
 void SoundPlayer::playDetecting() {
+	/*
 	isDetecting = true;
 	isNotifying = false;
 	if (soundArakawaWorld.getIsPlaying() == false) {
 		soundArakawaWorld.play();
 	}
+	*/
 }
 
 void SoundPlayer::stopDetecting() {
+	/*
 	isDetecting = false;
 	if (soundArakawaWorld.volume < 0.05) {
 		soundArakawaWorld.stop();
 	}
+	*/
 }
